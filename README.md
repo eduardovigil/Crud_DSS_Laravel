66aac01af405c51807b8a3a52fb885c520b48a50
eduardovigil <54220799+eduardovigil@users.noreply.github.com>
2023-04-11 23:12:22 -0600

    Update all crud this apirest in laravel

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# API REST LARAVEL
+
+Universidad Don Bosco
+
+Materia:
+Desarrollo de aplicaciones Web con software interpretados en el servidor
+
+Alumnos:
+David Eduardo Rodriguez Vigil RV202840
+Carlos Leandro Garcia Mena    GM222754
+Jefferson Alexis Castro Castro CC222749
+Jose Guillermo Granadino Rivas GR222755
+Edmilson Alejandro Martinez Reynosa MR222768
+
+
---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Employee;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $employee = Employee::all();
+        return \response($employee);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre'=> 'required',
+            'apellido'=> 'required',
+            'edad'=> 'required',
+            'salario'=> 'required',
+        ]);
+        $employee = Employee::create($request->all());
+
+        return \response($employee);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $employee = Employee::findOrFail($id);
+        return \response($employee);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $employee = Employee::findOrFail($id)->update($request->all());
+        return \response("Sea actualizado la informacion del empleado");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $employee = Employee::destroy($id);
+        return \response("El empleado de id $id sea eliminado");
+    }
+}
